79ba08ea735eac2b5469e98831e06d040a8a1e13
Fidel <fidel.angel@enigmatech.io>
2023-06-01 08:47:49 -0600

    Locales Support

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,9 @@ import {
   StatusBar
 } from 'react-native';
 
+import "./locales/i18n"
+import { useTranslation } from 'react-i18next';
+
 import { useSelector, useDispatch } from 'react-redux';
 
 import { useTheme } from 'react-native-paper';
@@ -15,20 +18,18 @@ import {
   init
 } from './redux/app/reducers';
 
+
 function App() {
 
   const theme = useTheme();
 
   const dispatch = useDispatch();
-  const app = useSelector(state => state.app);
 
   const initApp = () => dispatch(init());
 
   React.useEffect(() => {
     initApp();
   }, []);
-
-  console.log(app)
 
   return (
     <SafeAreaView style={{ backgroundColor: theme.colors.background, flex: 1 }} >
